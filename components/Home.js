3a49fd2276f8cf94595b9cddbfa147a0a5b4aaa0
ErgoMundusDovi <eddylou91@gmail.com>
2025-07-09 14:00:17 +0200

    arianne...

--- components/Home.js
+++ components/Home.js
@@ -38,7 +38,7 @@ function Home() {
   // Movies list
 
 useEffect(()=>{
-  fetch('http://localhost:3000/movies')
+  fetch('https://mymovies-back-seven.vercel.app/movies')
   .then(response=>response.json())
   .then(data=>{
     setMoviesData(data.movies)})
